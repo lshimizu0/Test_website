2385ef037ebc38eadc3853c19fa4de2891a9f766
lshimizu0 <lupshimizu@gmail.com>
2025-10-14 15:34:54 -0600

    sql connection user and pass established

--- process-form.php
+++ process-form.php
@@ -1,3 +1,10 @@
 <?php
 
-print_r($_POST);
+$username = $_POST['username'];
+$password = $_POST['password'];
+$terms_and_conditions = filter_input(INPUT_POST, 'terms-and-conditions', FILTER_VALIDATE_BOOLEAN);
+
+$host = "localhost";
+$dbname ="accounts";
+$server_username = "ucalgary";
+$server_password = "oop";
